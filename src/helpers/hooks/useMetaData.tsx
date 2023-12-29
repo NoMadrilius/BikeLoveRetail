258355a3ce2e5cad616dff94874afcda246e1d13
Sergei Shtil <91010230+sergei22dj@users.noreply.github.com>
2023-12-29 16:49:22 +0200

    | image card |

--- src/helpers/hooks/useMetaData.tsx
+++ src/helpers/hooks/useMetaData.tsx
@@ -1,22 +1,22 @@
 import Head from "next/head";
 import { FC } from "react";
 type Props = {
-  title: string;
-  img: string;
-  description: string;
+	title: string;
+	img: string;
+	description: string;
 };
 export const UseMetaData: FC<Props> = ({ title, img, description }) => {
-  return (
-    <Head>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <meta property="og:title" content={title} />
-      <meta property="og:description" content={description} />
-      <meta property="og:image" content={img} />{" "}
-      <meta name="twitter:title" content={title} />
-      <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={img} />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-  );
+	return (
+		<Head>
+			<title>{title}</title>
+			<meta name='description' content={description} />
+			<meta property='og:title' content={title} />
+			<meta property='og:description' content={description} />
+			<meta property='og:image' content={"/mock/NoPhoto.png"} />{" "}
+			<meta name='twitter:title' content={title} />
+			<meta name='twitter:description' content={description} />
+			<meta name='twitter:image' content={"/mock/NoPhoto.png"} />
+			<link rel='icon' href='/favicon.ico' />
+		</Head>
+	);
 };
