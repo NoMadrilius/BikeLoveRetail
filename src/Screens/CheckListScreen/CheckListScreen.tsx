5f6c512bc95407ec8abbfce930138b04c5f21bd9
Sergei Shtil <91010230+sergei22dj@users.noreply.github.com>
2024-01-07 14:34:00 +0200

    | get OrderPage data from cart |

--- src/Screens/CheckListScreen/CheckListScreen.tsx
+++ src/Screens/CheckListScreen/CheckListScreen.tsx
@@ -8,76 +8,125 @@ import Registration from "./components/Registration";
 import DeliveryInfo from "./components/DeliveryInfo";
 import PayInfo from "./components/PayInfo";
 import ListItems from "./components/ListItems";
+import { useCartStore } from "@/store/CartStore";
+import { useEffect, useState } from "react";
+import { IProduct } from "@/types/types";
+import { prettyPrice } from "@/helpers/stringDecorate/stringDecorate";
 
 const CheckListScreen = () => {
-  const road = [
-    { title: "Корзина", link: "/" },
-    { title: "Оформление заказа", link: "/" },
-  ];
+	const road = [
+		{ title: "Корзина", link: "/" },
+		{ title: "Оформление заказа", link: "/" },
+	];
 
-  return (
-    <>
-      <UseMetaData
-        title={"Оформление заказа"}
-        img={""}
-        description={"толдфыад"}
-      />
-      <Wrapper>
-        <BreadCrumbs road={road} />
+	const cartStore = useCartStore();
+	const [cart, setCart] = useState<IProduct[]>([]);
 
-        <Container>
-          <Left>
-            <Registration />
-            <DeliveryInfo />
-            <PayInfo />
-          </Left>
-          <Line />
-          <Right>
-            <ListItems />
-          </Right>
-        </Container>
-      </Wrapper>
-    </>
-  );
+	useEffect(() => {
+		setCart(cartStore.cart);
+	}, []);
+	return (
+		<>
+			<UseMetaData
+				title={"Оформление заказа"}
+				img={""}
+				description={"толдфыад"}
+			/>
+			<Wrapper>
+				<BreadCrumbs road={road} />
+
+				<Container>
+					<Left>
+						<Registration />
+						<DeliveryInfo />
+						<PayInfo />
+					</Left>
+					<Line />
+					<Right>
+						<Text color={colors.black} size='22px' fontStyle={fonts.f600}>
+							СОСТАВ ЗАКАЗА
+						</Text>
+						{cart && (
+							<>
+								{cart?.map((el, index) => (
+									<ListItems data={el} key={index} />
+								))}
+							</>
+						)}
+						<TotalPrice>
+							<Text
+								color={colors.black}
+								size='18px'
+								fontStyle={fonts.f400}
+								margin='0 auto 0 0'>
+								ИТОГО:
+							</Text>
+							<Text
+								color={colors.black}
+								size='18px'
+								fontStyle={fonts.f400}
+								margin='0 0 0 auto'>
+								{prettyPrice(
+									cart.reduce(
+										(acc, item) => acc + item.retailPrice * item.quantity,
+										0
+									)
+								)}{" "}
+								UAH
+							</Text>
+						</TotalPrice>
+					</Right>
+				</Container>
+			</Wrapper>
+		</>
+	);
 };
 export default CheckListScreen;
+//
+const TotalPrice = styled.div`
+	display: flex;
+	padding: 35px 39px;
+	background-color: ${colors.redBlur};
+	border-radius: 15px;
+	margin-top: 60px;
+`;
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 50px;
+	display: flex;
+	flex-direction: column;
+	margin-bottom: 50px;
 `;
 const Container = styled.div`
-  display: flex;
-  column-gap: 23px;
-  @media (max-width: 1245px) {
-    flex-direction: column-reverse;
-    align-items: center;
-    row-gap: 23px;
-  }
+	display: flex;
+	column-gap: 23px;
+	@media (max-width: 1245px) {
+		flex-direction: column-reverse;
+		align-items: center;
+		row-gap: 23px;
+	}
 `;
 const Left = styled.div`
-  display: flex;
-  flex-direction: column;
-  row-gap: 40px;
-  width: 55%;
-  @media (max-width: 1245px) {
-    width: 100%;
-  }
+	display: flex;
+	flex-direction: column;
+	row-gap: 40px;
+	width: 55%;
+	@media (max-width: 1245px) {
+		width: 100%;
+	}
 `;
 const Right = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 45%;
-  @media (max-width: 1245px) {
-    width: 100%;
-  }
+	display: flex;
+	flex-direction: column;
+	width: 45%;
+	@media (max-width: 1245px) {
+		width: 100%;
+	}
 `;
 const Line = styled.div`
-  width: 1px;
-  height: 1327px;
-  background-color: ${colors.grayBorder};
-  @media (max-width: 1245px) {
-    display: none;
-  }
+	width: 1px;
+	height: 1327px;
+	background-color: ${colors.grayBorder};
+	@media (max-width: 1245px) {
+		display: none;
+	}
 `;
--- src/Screens/CheckListScreen/components/ListItems.tsx
+++ src/Screens/CheckListScreen/components/ListItems.tsx
@@ -2,68 +2,73 @@ import { Text } from "@/components/Text/Text";
 import { styled } from "styled-components";
 import { colors } from "../../../../theme/colors";
 import { fonts } from "../../../../theme/fonts";
+import { IProduct } from "@/types/types";
+import { FC } from "react";
+import { prettyPrice } from "@/helpers/stringDecorate/stringDecorate";
 
-const ListItems = () => {
-  return (
-    <>
-      <Wrapper>
-        <Text color={colors.black} size="22px" fontStyle={fonts.f600}>
-          СОСТАВ ЗАКАЗА
-        </Text>
-        <ItemWrapper>
-          <Picture src="/mock/testCardByPropose.png" />
-          <ColumnContainer>
-            <Text color={colors.black} size="16px" fontStyle={fonts.f600}>
-              S-Works Tarmac SL7 - SRAM Red eTap AXS
-            </Text>
-            <Text color={colors.black} size="15px" fontStyle={fonts.f400}>
-              Размер: 28
-            </Text>
-            <RowContainer>
-              <Text color={colors.black} size="16px" fontStyle={fonts.f400}>
-                65 000 UAH
-              </Text>
-              <Text color={colors.black} size="16px" fontStyle={fonts.f400}>
-                2 шт
-              </Text>
-            </RowContainer>
-          </ColumnContainer>
-        </ItemWrapper>
-      </Wrapper>
-    </>
-  );
+type Props = {
+	data: IProduct;
+};
+
+const ListItems: FC<Props> = ({ data }) => {
+	console.log(data);
+	return (
+		<>
+			<Wrapper>
+				<ItemWrapper>
+					<Picture src={data.image || "/mock/NoPhoto.png"} />
+					<ColumnContainer>
+						<Text color={colors.black} size='16px' fontStyle={fonts.f600}>
+							{data.name}
+						</Text>
+						<Text color={colors.black} size='15px' fontStyle={fonts.f400}>
+							Размер:
+						</Text>
+						<RowContainer>
+							<Text color={colors.black} size='16px' fontStyle={fonts.f400}>
+								{prettyPrice(data.retailPrice)} UAH
+							</Text>
+							<Text color={colors.black} size='16px' fontStyle={fonts.f400}>
+								{data.quantity} шт
+							</Text>
+						</RowContainer>
+					</ColumnContainer>
+				</ItemWrapper>
+			</Wrapper>
+		</>
+	);
 };
 export default ListItems;
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-top: 20px;
+	display: flex;
+	flex-direction: column;
+	margin-top: 20px;
 `;
 const ItemWrapper = styled.div`
-  display: flex;
-  column-gap: 32px;
-  margin-top: 20px;
-  padding: 21px 0;
-  border-bottom: 1px solid ${colors.grayBorder};
+	display: flex;
+	column-gap: 32px;
+	margin-top: 20px;
+	padding: 21px 0;
+	border-bottom: 1px solid ${colors.grayBorder};
 `;
 const Picture = styled.img`
-  border: 1px solid ${colors.grayBorder};
-  border-radius: 10px;
-  width: 201px;
-  height: 137px;
-  @media (max-width: 756px) {
-    width: 123px;
-    height: 85px;
-  }
+	border: 1px solid ${colors.grayBorder};
+	border-radius: 10px;
+	width: 201px;
+	height: 137px;
+	@media (max-width: 756px) {
+		width: 123px;
+		height: 85px;
+	}
 `;
 const ColumnContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding-bottom: 20px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+	padding-bottom: 20px;
 `;
 const RowContainer = styled.div`
-  display: flex;
-  column-gap: 50px;
+	display: flex;
+	column-gap: 50px;
 `;
