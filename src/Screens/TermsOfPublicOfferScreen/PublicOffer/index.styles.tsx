5765403617577399f805e4969472b4604a360fea
Marjan <mlukavyi@gmail.com>
2024-02-12 03:07:59 +0200

    Add localization to "Публічна оферта" page

---
+++ src/Screens/TermsOfPublicOfferScreen/PublicOffer/index.styles.tsx
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+export const PublicOfferContainer = styled.section`
+  color: rgb(16, 17, 18);
+  line-height: 150%;
+  padding-top: 50px;
+  padding-bottom: 71px;
+`;
+
+export const Title = styled.h1`
+  font-size: 18px;
+  margin-bottom: 10px;
+`;
+
+export const Subtitle = styled.h2`
+  font-size: 18px;
+  font-weight: 400;
+  margin: 10px 0;
+  text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const Paragraph = styled.p`
+  margin: 5px 0;
+  text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+`;
+
+export const List = styled.ol`
+  padding-left: 20px;
+`;
+
+export const ListItem = styled.li`
+  margin: 5px 0;
+  text-shadow: 0 4px 4px rgba(0, 0, 0, 0.25);
+`;
