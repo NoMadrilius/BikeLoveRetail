c2e9b0ecb3db237d31a56d1e516a3538491340f9
NoMadrilius <122482610+NoMadrilius@users.noreply.github.com>
2024-02-23 16:08:13 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -5,7 +5,7 @@ const RUN_MODE: RunMode = "PROD";
 const BASE_URL = {
   DEV: "https://bikeshop.1gb.ua/api",
   //PROD: "http://localhost:5000/api",
-  PROD: "http://api.bikelove.com.ua/api",
+  PROD: "https://api.bikelove.com.ua/api",
 };
 const IMG_URL = {
   DEV: "http://localhost:3000/",
