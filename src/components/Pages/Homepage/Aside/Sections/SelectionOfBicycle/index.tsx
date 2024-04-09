aedcd63d41773a04a4965bba9e93bdf3b086f9d1
Marjan <mlukavyi@gmail.com>
2024-04-10 00:07:39 +0300

    fix minor bugs

--- src/components/Pages/Homepage/Aside/Sections/SelectionOfBicycle/index.tsx
+++ src/components/Pages/Homepage/Aside/Sections/SelectionOfBicycle/index.tsx
@@ -90,42 +90,48 @@ const SelectionOfBicycle = ({ className }: { className?: string }) => {
   };
   return (
     <section
-      className={`w-fill rounded-lg flex flex-col bg-white p-5 gap-5 sm:mx-5 ${className}`}
+      className={`w-fill rounded-lg flex flex-col bg-white p-5 gap-5 sm:mx-5  md:flex md:flex-row md:p-0 md:bg-transparent ${className}`}
     >
-      <h2 className="text-dark-text text-[20px] leading-[120%] font-bold font-robot-c">
+      <h2 className="text-dark-text md:text-[32px] md:leading-[37.5px] md:font-medium text-[20px] leading-[120%] font-bold font-robot-c md:max-w-[175px]">
         Підбір велосипеда
       </h2>
-      <Dropdown label="Оберіть тип" options={options} onSelect={handleSelect} />
-      <Dropdown
-        label="Оберіть зріст, см"
-        options={options}
-        onSelect={handleSelect}
-      />
-      <div>
-        <BicycleOption
-          label="Велосипеди від UAH"
-          onInputChange={() => {}}
-          placeholder="до 120 000 UAH"
+      <div className="flex flex-col gap-5 md:p-5 md:bg-white md:rounded-lg md:grow">
+        <Dropdown
+          label="Оберіть тип"
+          options={options}
+          onSelect={handleSelect}
         />
-        <BicycleOption
-          label="Велосипеди до UAH"
-          onInputChange={() => {}}
-          placeholder="до 120 000 UAH"
+        <Dropdown
+          label="Оберіть зріст, см"
+          options={options}
+          onSelect={handleSelect}
         />
-      </div>
-      <BicycleOption label="Додати бренд" onInputChange={() => {}} />
-      <GradientButton
-        label={"Показати"}
-        showIcon={false}
-        className="w-full flex !py-[14.5px] justify-center"
-      />
-      <div className="mx-auto flex">
-        <span className="font-light text-[14px] leading-[19.2px] text-dark font-inter">
-          Знайдено товарів{" "}
-          <span className="font-semibold leading-[19.36px] text-[16px] text-dark">
-            1203
+        <div>
+          <BicycleOption
+            label="Велосипеди від UAH"
+            onInputChange={() => {}}
+            placeholder="до 120 000 UAH"
+          />
+          <BicycleOption
+            label="Велосипеди до UAH"
+            onInputChange={() => {}}
+            placeholder="до 120 000 UAH"
+          />
+        </div>
+        <BicycleOption label="Додати бренд" onInputChange={() => {}} />
+        <GradientButton
+          label={"Показати"}
+          showIcon={false}
+          className="w-full flex !py-[14.5px] justify-center"
+        />
+        <div className="mx-auto flex">
+          <span className="font-light text-[14px] leading-[19.2px] text-dark font-inter">
+            Знайдено товарів{" "}
+            <span className="font-semibold leading-[19.36px] text-[16px] text-dark">
+              1203
+            </span>
           </span>
-        </span>
+        </div>
       </div>
     </section>
   );
--- src/components/UIKit/Cards/ProductCard/ProductImage/index.tsx
+++ src/components/UIKit/Cards/ProductCard/ProductImage/index.tsx
@@ -8,9 +8,10 @@ const ProductImage = ({ classname }: ProductImageProps) => {
   return (
     <div
       className={`relative w-full h-[128px] 
-      md:w-[276px] md:h-[194px] 
+      md:w-full md:max-w-[276px] md:h-[194px] 
       xl:w-full xl:h-[194px] 
-      sm:mb-[9px] mb-5 xl:mb-[13px] ${classname}`}
+      sm:mb-[9px] mb-5 xl:mb-[13px] 
+      ${classname}`}
     >
       <Image
         src={
