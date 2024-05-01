fb8dd51c224286df6410796d2ed1aa4dacca7abd
NoMadrilius <122482610+NoMadrilius@users.noreply.github.com>
2024-05-01 11:13:17 +0300

    Update [[...slug]].tsx

--- src/pages/catalog/[[...slug]].tsx
+++ src/pages/catalog/[[...slug]].tsx
@@ -13,7 +13,7 @@ import {GetCatalogLinkParams} from "@/helpers/GetCatalogLinkParams";
 
 export const getStaticPaths = async () => {
   // Fetch the dynamic paths from your API or any data source
-  const paths = ["/catalog/velosipedi/id=1"];
+  const paths = [];
 
   return {
     paths,
