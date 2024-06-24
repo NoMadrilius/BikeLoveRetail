4efb9bfdee8833ba80cd23be8654173078df3174
NoMadrilius <122482610+NoMadrilius@users.noreply.github.com>
2024-06-24 21:27:39 +0300

    fix

--- src/pages/gratitude/[id].tsx
+++ src/pages/gratitude/[id].tsx
@@ -6,6 +6,15 @@ import {useCurrencyStore} from "@/store/CurrencyStore";
 import {useBikeSelectionStore} from "@/store/BikeSelectionStore";
 import {AppState} from "@/dataTransferObjects/internal/AppState";
 
+export const getStaticPaths = async () => {
+  // Fetch the dynamic paths from your API or any data source
+  const paths = [] as string[];
+
+  return {
+    paths,
+    fallback: true // or 'blocking' if you want to use incremental static regeneration
+  };
+};
 export async function getStaticProps() {
   return {props: {as:await loadAppState()}, revalidate:60}
 }
