04ce8f57df354a1d786f414e155560cb6920e23f
NoMadrilius <evgennomad@gmail.com>
2025-10-09 01:40:15 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-next.config.js/** @type {import('next').NextConfig} */
+/** @type {import('next').NextConfig} */
 const { i18n } = require('./next-i18next.config')
 
 const nextConfig = {
@@ -31,6 +31,7 @@ const nextConfig = {
       },
     ];
   },
+
   images: {
     domains: ["www.statebicycle.com", "storage.googleapis.com","res.cloudinary.com","s3-alpha-sig.figma.com","s3-alpha-sig.figma.com","img.youtube.com"],
   },
